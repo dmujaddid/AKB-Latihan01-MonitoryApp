bd76c97ff22b02f44c4d36a67f38793a79a12103
Eka <obyprof@gmail.ccom>
2020-04-09 07:00:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/monitoryapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.monitoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+// tanggal pengerjaan  : Rabu, 8 April 2020
+// deskripsi pekerjaan : mengerjakan halaman awal (background, tombol)
+// nim                 : 10116599
+// nama                : Robby Eka
+// kelas               : IF6K
